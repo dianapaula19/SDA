96b2518f3daae2176cff3afc83312ad218c86ba0
Diana <dianabacircea19@gmail.com>
2019-11-25 23:42:46 +0200

    tema 4

--- teme/tema_4/tema_4_3/main.cpp
+++ teme/tema_4/tema_4_3/main.cpp
@@ -4,32 +4,28 @@ using namespace std;
 struct node{
 
     int value;
+    int left_size;
     node* left;
     node* right;
 
 };
-class binarySearchTree{
+class binaryIndexedTree{
 
 public:
     node *root;
-    binarySearchTree();
+    binaryIndexedTree();
     void insert_node(int);
-    int search_node(int);
-    int findMax();
-    node* findMinNode(node*);
-    static void RSD(node*);
+    static int calculateLeftSize(node*);
+    static int searchIndex(node*, int);
     static void SRD(node*);
-    static void SDR(node*);
-    static void printBetweenFirstKeyAndSecondKey(node*, int, int);
-    node* delete_node(node*, int);
-    friend istream& operator >> (istream&, binarySearchTree&);
+    friend istream& operator >> (istream&, binaryIndexedTree&);
 };
-binarySearchTree::binarySearchTree(){
+binaryIndexedTree::binaryIndexedTree(){
 
     root = NULL;
 
 }
-void binarySearchTree::insert_node(int a){
+void binaryIndexedTree::insert_node(int a){
 
     node* q = new node;
     q->value = a;
@@ -71,114 +67,44 @@ void binarySearchTree::insert_node(int a){
     }
 
 }
-int binarySearchTree::search_node(int a){
+binaryIndexedTree::calculateLeftSize(node* root_sub_tree) {
 
-    if(root == NULL) {
+    int number = 1;
+    if(root_sub_tree == NULL) {
         return 0;
-    }
-    node* p = root;
-    if(p->value == a) {
-        return 1;
     } else {
-        while(p) {
-            if(p->value == a) {
-                return 1;
-            } else if (p->value > a) {
-                p = p->left;
-            } else {
-                p = p->right;
-            }
-        }
+        number += binaryIndexedTree::calculateLeftSize(root_sub_tree->left);
+        number += binaryIndexedTree::calculateLeftSize(root_sub_tree->right);
+        return number;
+    }
+
+}
+binaryIndexedTree::searchIndex(node* root_, int index) {
+
+    if(index == root_->left_size) {
+        return root_->value;
+    } else if (index < root_->left_size) {
+        return searchIndex(root_->left, index);
+    } else if (index > root_->left_size) {
+        return index - root_->left_size - 1;
     }
     return 0;
 
 }
-int binarySearchTree::findMax(){
-
-    node* p = root;
-    while(p->right != NULL){
-        p = p->right;
-    }
-    return p->value;
-
-}
-node* binarySearchTree::findMinNode(node* root_) {
-
-    node* p = root_;
-    while(p && p->left != NULL) {
-        p = p->left;
-    }
-    return p;
-
-}
-void binarySearchTree::RSD(node* root_){
+void binaryIndexedTree::SRD(node* root_){
 
     if(root_) {
-    cout << root_->value << " ";
-    binarySearchTree::RSD(root_->left);
-    binarySearchTree::RSD(root_->right);
-    }
-
-}
-void binarySearchTree::SRD(node* root_){
-
-    if(root_) {
-        binarySearchTree::SRD(root_->left);
-        cout << root_->value << " ";
-        binarySearchTree::SRD(root_->right);
-    }
-}
-void binarySearchTree::SDR(node* root_){
-
-    if(root_) {
-        binarySearchTree::SDR(root_->left);
-        binarySearchTree::SDR(root_->right);
-        cout << root_->value << " ";
-    }
-}
-void binarySearchTree::printBetweenFirstKeyAndSecondKey(node* root_, int key_1, int key_2) {
-
-    if(root_ == NULL) {
-        return;
-    }
-    if(root_->value > key_1) {
-        printBetweenFirstKeyAndSecondKey(root_->left, key_1, key_2);
-    }
-    if(root_->value >= key_1 && root_->value <= key_2) {
-        cout << root_->value << " ";
-    }
-    if(root_->value < key_2) {
-        printBetweenFirstKeyAndSecondKey(root_->right, key_1, key_2);
-    }
-
-}
-node* binarySearchTree::delete_node(node* root_, int a) {
-
-    if(root_ ==  NULL) {
-        return root_;
-    }
-    if(a < root_->value) {
-        root_->left = binarySearchTree::delete_node(root_->left, a);
-    } else if(a > root_->value) {
-        root_->right = binarySearchTree::delete_node(root_->right, a);
-    } else {
-        if(root_->left == NULL) {
-            node* temp = root_->right;
-            delete root_;
-            return temp;
-        } else if(root_->right == NULL) {
-            node* temp = root_->left;
-            delete root_;
-            return temp;
+        binaryIndexedTree::SRD(root_->left);
+        if(root_->left != NULL) {
+            root_->left_size = binaryIndexedTree::calculateLeftSize(root_->left);
+        } else {
+            root_->left_size = 0;
         }
-        node* temp = binarySearchTree::findMinNode(root_->right);
-        root_->value = temp->value;
-        root_->right = delete_node(root_->right, temp->value);
+        cout << root_->value << " " << root_->left_size << "\n";
+        binaryIndexedTree::SRD(root_->right);
     }
-    return root_;
-
 }
-istream& operator >> (istream& in, binarySearchTree& bst){
+istream& operator >> (istream& in, binaryIndexedTree& bit){
 
     cout << "Numarul de elemente din arbore" << endl;
     int n;
@@ -186,13 +112,19 @@ istream& operator >> (istream& in, binarySearchTree& bst){
     for(int i = 0; i < n; ++i) {
         int new_value;
         cin >> new_value;
-        bst.insert_node(new_value);
+        bit.insert_node(new_value);
     }
     return in;
 
 }
 int main()
 {
-    cout << "Hello world!" << endl;
+    binaryIndexedTree bit;
+    cin >> bit;
+    binaryIndexedTree::SRD(bit.root);
+    cout << "Pozitia este:\n";
+    int k;
+    cin >> k;
+    cout << binaryIndexedTree::searchIndex(bit.root, k);
     return 0;
 }
